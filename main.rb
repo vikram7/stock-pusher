430bd2d2ae88dc5a9e8e2b632afdb5f553780428
Vikram Ramakrishnan <lord.ezar@gmail.com>
2015-01-18 14:59:15 -0500

    rename keys

--- main.rb
+++ main.rb
@@ -1,20 +1,30 @@
 require 'uri'
 require 'net/https'
 require 'json'
+require 'firebase'
 require_relative 'tradier_api'
+
 
 securities = %w[aapl googl msft crm vmw]
 
-hash = Hash.new
-array = Array.new
 securities.each do |security|
+  start = Time.now
+  hash = Hash.new
   stock_data = TradierApi.new(security)
   hash =
     {
-      "ticker" => stock_data.ticker,
-      "last" => stock_data.last,
-      "last_volume" => stock_data.last_volume,
-      "timestamp" => stock_data.timestamp
+      :symbol => stock_data.symbol,
+      :price => stock_data.price,
+      :volume => stock_data.volume,
+      :timestamp => stock_data.timestamp
     }
-  array << hash
+
+  base_uri = 'https://stock-indicator.firebaseio.com/'
+  firebase = Firebase::Client.new(base_uri)
+  puts "======="
+  puts Time.now - start
+  response = firebase.push("stocks", hash)
+  puts Time.now - start
+  puts "======="
 end
+puts Time.now - start
--- tradier_api.rb
+++ tradier_api.rb
@@ -1,5 +1,5 @@
 class TradierApi
-  attr_accessor :ticker, :last, :last_volume, :timestamp
+  attr_accessor :symbol, :price, :volume, :timestamp
   def initialize(underlying)
     uri = URI.parse("https://sandbox.tradier.com/v1/markets/quotes?symbols=#{underlying}")
     http = Net::HTTP.new(uri.host, uri.port)
@@ -14,9 +14,9 @@ class TradierApi
     underlying_data = http.request(request)
     parsed_underlying_data = JSON.parse(underlying_data.body)
     underlying_data = parsed_underlying_data["quotes"]["quote"]
-    @ticker = underlying_data["symbol"]
-    @last = underlying_data["last"]
-    @last_volume = underlying_data["last_volume"]
+    @symbol = underlying_data["symbol"]
+    @price = underlying_data["last"]
+    @volume = underlying_data["last_volume"]
     @timestamp = underlying_data["trade_date"]
   end
 end
